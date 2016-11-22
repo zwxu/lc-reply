202361d38980a34fc326e89e1126449c41fc6da9
zwxu <xzw_6773@qq.com>
2016-11-22 22:50:24 +0800

    reply

--- lc-reply-web/src/main/java/com/ustc/zwxu/lc/reply/web/controller/UserController.java
+++ lc-reply-web/src/main/java/com/ustc/zwxu/lc/reply/web/controller/UserController.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,10 +29,19 @@ public class UserController {
 	private UserQueryService userQueryService;
 	
 	@RequestMapping("/list")
-	public String list(HttpServletRequest request, HttpServletResponse response, @PathVariable Map<String, String> vars) {
+	public String list(HttpServletRequest request, HttpServletResponse response, ModelMap model,@PathVariable Map<String, String> vars) {
 		HttpSession session = request.getSession();
-		List<UserInfo> list=userQueryService.query(1);
-		logger.info("helll: "+list);
+		String start = request.getParameter("start");
+		if(start ==null)
+		{
+			start=String.valueOf(1);
+		}
+		int limit = 2;
+		
+		model.addAttribute("limit", limit) ;
+		model.addAttribute("total", 6) ;
+		model.addAttribute("start", Integer.parseInt(start));
+		List<UserInfo> list=userQueryService.queryPage(Integer.parseInt(start),limit);
 		session.setAttribute("userList", list);
 		return "allUser";
 	}
