12bc3a11d383872c80499174631dec44c3f029e5
xuzhiwei <xuzhiwei@xiaomi.com>
2016-11-24 11:04:13 +0800

    modify sql

--- lc-reply-web/src/main/resources/config/ustc.sql
+++ lc-reply-web/src/main/resources/config/ustc.sql
@@ -10,25 +10,33 @@ Target Server Type    : MYSQL
 Target Server Version : 50631
 File Encoding         : 65001
 
-Date: 2016-11-23 17:59:48
+Date: 2016-11-24 11:03:40
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for t_admin
+-- Table structure for t_account
 -- ----------------------------
-DROP TABLE IF EXISTS `t_admin`;
-CREATE TABLE `t_admin` (
-  `id` int(11) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `deparment` varchar(255) DEFAULT NULL,
-  `created_time` datetime DEFAULT NULL
+DROP TABLE IF EXISTS `t_account`;
+CREATE TABLE `t_account` (
+  `id` int(11) NOT NULL,
+  `loginname` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `rid` int(10) DEFAULT '3',
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of t_admin
+-- Records of t_account
 -- ----------------------------
+INSERT INTO `t_account` VALUES ('1', 'haha', '123456', '3', '2016-09-22 17:15:27');
+INSERT INTO `t_account` VALUES ('2', 'sss', '123456', '3', '2016-09-22 17:15:27');
+INSERT INTO `t_account` VALUES ('3', 'sss', 'haha', '3', '2016-09-23 18:12:00');
+INSERT INTO `t_account` VALUES ('4', 'sss', 'haha', '3', '2016-09-23 18:12:00');
+INSERT INTO `t_account` VALUES ('5', 'sss', 'haha', '3', '2016-09-23 18:12:00');
+INSERT INTO `t_account` VALUES ('6', 'sss', 'haha', '3', '2016-09-23 18:12:00');
 
 -- ----------------------------
 -- Table structure for t_menu
@@ -38,12 +46,14 @@ CREATE TABLE `t_menu` (
   `id` int(11) DEFAULT NULL,
   `menu_name` varchar(255) DEFAULT NULL,
   `menu_url` varchar(255) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
   `parent_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_menu
 -- ----------------------------
+INSERT INTO `t_menu` VALUES ('1', '学生列表', '/userList', '1', null);
 
 -- ----------------------------
 -- Table structure for t_role
@@ -74,6 +84,7 @@ CREATE TABLE `t_student` (
   `school` varchar(255) DEFAULT NULL,
   `number` int(10) DEFAULT NULL,
   `groupid` int(10) DEFAULT NULL,
+  `aid` int(11) DEFAULT NULL,
   `tid` int(10) DEFAULT NULL,
   `pass_count` int(10) DEFAULT NULL,
   `reject_count` int(10) DEFAULT NULL,
@@ -85,32 +96,27 @@ CREATE TABLE `t_student` (
 -- ----------------------------
 -- Records of t_student
 -- ----------------------------
-INSERT INTO `t_student` VALUES ('1', 'haha', null, '0', null, '1', null, null, null, '2016-09-22 17:15:27');
-INSERT INTO `t_student` VALUES ('2', 'sss', '', '0', null, '1', null, null, '', '2016-09-22 17:15:27');
-INSERT INTO `t_student` VALUES ('3', 'sss', '', null, '0', null, '1', null, '', '2016-09-23 18:12:00');
-INSERT INTO `t_student` VALUES ('4', 'sss', '', null, '0', null, '1', null, '', '2016-09-23 18:12:00');
-INSERT INTO `t_student` VALUES ('5', 'sss', '', null, '0', null, '1', null, '', '2016-09-23 18:12:00');
-INSERT INTO `t_student` VALUES ('6', 'sss', '', null, '0', null, '1', null, '', '2016-09-23 18:12:00');
+INSERT INTO `t_student` VALUES ('1', 'haha', null, '0', null, null, '1', null, null, null, '2016-09-22 17:15:27');
+INSERT INTO `t_student` VALUES ('2', 'sss', '', '0', null, null, '1', null, null, '', '2016-09-22 17:15:27');
+INSERT INTO `t_student` VALUES ('3', 'sss', '', null, '0', null, null, '1', null, '', '2016-09-23 18:12:00');
+INSERT INTO `t_student` VALUES ('4', 'sss', '', null, '0', null, null, '1', null, '', '2016-09-23 18:12:00');
+INSERT INTO `t_student` VALUES ('5', 'sss', '', null, '0', null, null, '1', null, '', '2016-09-23 18:12:00');
+INSERT INTO `t_student` VALUES ('6', 'sss', '', null, '0', null, null, '1', null, '', '2016-09-23 18:12:00');
 
 -- ----------------------------
--- Table structure for t_user
+-- Table structure for t_teacher
 -- ----------------------------
-DROP TABLE IF EXISTS `t_user`;
-CREATE TABLE `t_user` (
-  `id` int(11) NOT NULL,
-  `loginname` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `role_id` int(10) DEFAULT '3',
-  `create_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS `t_teacher`;
+CREATE TABLE `t_teacher` (
+  `id` int(11) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `deparment` varchar(255) DEFAULT NULL,
+  `aid` int(11) DEFAULT NULL,
+  `created_time` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of t_user
+-- Records of t_teacher
 -- ----------------------------
-INSERT INTO `t_user` VALUES ('1', 'haha', '123456', '3', '2016-09-22 17:15:27');
-INSERT INTO `t_user` VALUES ('2', 'sss', '123456', '3', '2016-09-22 17:15:27');
-INSERT INTO `t_user` VALUES ('3', 'sss', 'haha', '3', '2016-09-23 18:12:00');
-INSERT INTO `t_user` VALUES ('4', 'sss', 'haha', '3', '2016-09-23 18:12:00');
-INSERT INTO `t_user` VALUES ('5', 'sss', 'haha', '3', '2016-09-23 18:12:00');
-INSERT INTO `t_user` VALUES ('6', 'sss', 'haha', '3', '2016-09-23 18:12:00');
+INSERT INTO `t_teacher` VALUES ('1', '管理员1', '教学部', null, '2016-11-24 10:45:01');
+INSERT INTO `t_teacher` VALUES ('2', '老师1', '计算机学院', null, '2016-11-24 10:45:24');
