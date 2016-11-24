cde44e6d8212dd9a22c720ae51337468c44b6665
xuzhiwei <xuzhiwei@xiaomi.com>
2016-11-24 14:36:40 +0800

    delete

--- lc-reply-web/src/main/java/com/ustc/zwxu/lc/reply/web/controller/AccountController.java
+++ lc-reply-web/src/main/java/com/ustc/zwxu/lc/reply/web/controller/AccountController.java
@@ -1,21 +1,17 @@
 package com.ustc.zwxu.lc.reply.web.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
-import java.util.Map;
 
+import java.util.Map;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 
 import com.zwxu.lc.um.bean.QueryResult;
 import com.zwxu.lc.um.api.AccountQueryService;
