2e64ef19f6d17fd4e6a65ed2185f0e7a59c9aefa
xuzhiwei <xuzhiwei@xiaomi.com>
2016-12-21 10:35:01 +0800

    add file

---
+++ lc-reply-web/src/main/java/com/ustc/zwxu/lc/reply/web/test/Main.java
@@ -0,0 +1,21 @@
+package com.ustc.zwxu.lc.reply.web.test;
+
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+
+import com.zwxu.lc.um.api.StudentQueryService;
+import com.zwxu.lc.um.bean.QueryStudentResult;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JaxWsProxyFactoryBean factory=new JaxWsProxyFactoryBean();  
+        factory.setServiceClass(StudentQueryService.class);  
+          
+        factory.setAddress("http://localhost:8082/lc-um-core/services/studentQueryService");  
+        StudentQueryService service=(StudentQueryService)factory.create();  
+        QueryStudentResult result = service.queryAll();
+        System.out.println(result.getCount());
+	}
+
+}
